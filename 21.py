2ce4c3ac99bb6e38bb9c1556d1c5219033cff615
ArthurBGomes <brito.gomes@escolar.ifrn.edu.br>
2025-09-16 14:29:37 -0300

    Create 21.py

---
+++ 21.py
@@ -0,0 +1,23 @@
+# @cikey d3672fb6624b6c9bd68033513ef13975
+# @sid 20251174010013
+# @aid V2.1
+
+
+#begin_inputs
+
+# defina as variáveis aqui
+x = 1
+y = 2
+z = 3
+a = 4
+b = 5
+c = 6
+
+#end_inputs
+
+
+#os prints vão a partir daqui
+- print("{} e {} sao {} e {}".format(x , y, a, b))
+- print("Total:{}".format(x,y))
+- print("{} {}, {}, são números inteiros".format(a,b,c))
+- print("{}+ 0 =".format(z))
