09e6d90c7ce5ed1d96e2056339af6c6d56cac22f
ArthurBGomes <brito.gomes@escolar.ifrn.edu.br>
2025-09-16 13:52:24 -0300

    Create 0.py

---
+++ 0.py
@@ -0,0 +1,16 @@
+# @cikey d3672fb6624b6c9bd68033513ef13975
+# @sid 20251174010013
+# @aid 0
+
+
+#begin_inputs
+
+# Defina as variáveis de entrada aqui
+# Não apague as linhas com #begin_inputs e #end_inputs
+num1 =int(input("digite seu numero:"))
+num2 =int(input("digite seu numero:"))
+soma = num1 + num2
+#end_inputs
+
+# Faça o processamento e a exibição da saída a partir daqui
+print(soma)
