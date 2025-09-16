bd7b23201a917a4ab9a16a8bdbe294faec2ad290
ArthurBGomes <brito.gomes@escolar.ifrn.edu.br>
2025-09-16 13:59:00 -0300

    Create 11.py

---
+++ 11.py
@@ -0,0 +1,23 @@
+# @cikey d3672fb6624b6c9bd68033513ef13975
+# @sid 20251174010013
+# @aid V1.1
+
+
+#begin_inputs
+
+# As variáveis de entrada (os numeros, neste caso) deverão ser definidos nessa área 
+# entre #begin_inputs e #end_inputs.
+num4 = 4
+num6 = 6
+num20 = 20
+soma = num4 + num6
+subtracao = num6 - num4
+multiplicacao = num4 * num6
+divisao = num20 / num6
+diint =  num20 // num6
+resto = num20 % num6
+
+# O restante do código deve estar após o #end_inputs
+print(soma,subtracao,multiplicacao,divisao,diint,resto)
+#end_inputs
+
