dcedab4ee07a1cc006a02324c6715125458bb2e2
ArthurBGomes <brito.gomes@escolar.ifrn.edu.br>
2025-09-16 14:10:28 -0300

    Update 11.py

--- 11.py
+++ 11.py
@@ -10,14 +10,26 @@
 num4 = 4
 num6 = 6
 num20 = 20
-soma = num4 + num6
-subtracao = num6 - num4
-multiplicacao = num4 * num6
-divisao = num20 / num6
-diint =  num20 // num6
-resto = num20 % num6
+
+
+
+
+
+
 
 # O restante do código deve estar após o #end_inputs
-print(soma,subtracao,multiplicacao,divisao,diint,resto)
+soma = num4 + num6
+print("a soma:",soma)
+subtracao = num6 - num4
+print("a subtraçao:",subtracao)
+multiplicacao = num4 * num6
+print("a multiplicacao",multiplicacao)
+divisao = num20 / num6
+print("a divisao:",divisao)
+diint =  num20 // num6
+print("a diint:",diint)
+resto = num20 % num6
+print("o resto:",resto)
+
 #end_inputs
 
