48abb4cd99bc1f8a59272beff1011900f64334e8
ArthurBGomes <brito.gomes@escolar.ifrn.edu.br>
2025-09-24 17:33:24 -0300

    Create 42.py

---
+++ 42.py
@@ -0,0 +1,14 @@
+# @cikey d3672fb6624b6c9bd68033513ef13975
+# @sid 20251174010013
+# @aid V4.2
+
+
+#begin_inputs
+ #mantenha esse trecho. o input será manipulado aqui.
+#end_inputs
+for x in range():
+    if x >= 18:
+        print("maior de idade")
+    else:
+        print("menor de idade")
+	
