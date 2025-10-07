2bf1922bf6812b2c8aa94a3a126f40cc28d25546
ArthurBGomes <brito.gomes@escolar.ifrn.edu.br>
2025-10-07 13:53:30 -0300

    Create 61.py

---
+++ 61.py
@@ -0,0 +1,18 @@
+# @cikey d3672fb6624b6c9bd68033513ef13975
+# @sid 20251174010013
+# @aid V6.1
+
+
+#begin_inputs
+
+#end_inputs
+
+	
+import random
+
+numeros = []
+for i in range(6):
+    numero = random.randint(1, 50)
+    numeros.append(numero)
+
+print("Números sorteados:", numeros)
