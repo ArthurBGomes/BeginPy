d450616b2a2dba6c3069372f96fb9b73a3e6593e
ArthurBGomes <brito.gomes@escolar.ifrn.edu.br>
2025-10-29 16:31:32 -0300

    up

---
+++ 74.py
@@ -0,0 +1,16 @@
+# @cikey d3672fb6624b6c9bd68033513ef13975
+# @sid 20251174010013
+# @aid V7.4
+
+nome = []
+tel = []
+
+for n in nome:
+    user = input["Nome:"]
+    num = input["numero:"]
+    c = {user: num}
+    tel.append(c)
+print(f"{tel}")
+
+
+
