68246cf9749d9e33d4ff86bcadab0f286cda4768
ArthurBGomes <brito.gomes@escolar.ifrn.edu.br>
2025-11-04 13:49:24 -0300

    up

--- 74.py
+++ 74.py
@@ -1,16 +1,16 @@
 # @cikey d3672fb6624b6c9bd68033513ef13975
 # @sid 20251174010013
 # @aid V7.4
+tel = {}
 
-nome = []
-tel = []
-
-for n in nome:
-    user = input["Nome:"]
-    num = input["numero:"]
-    c = {user: num}
-    tel.append(c)
+for n in range(3):
+    user = input("Nome:")
+    num = input("numero:")
+    tel[user] = num    
 print(f"{tel}")
-
-
+wanted = input("nome que vc quer:")
+if wanted in tel:
+    print(f"o nome {wanted} está na lista de telefone{tel[user]}")
+else:
+    print("não tem esse contato na lista")
 
